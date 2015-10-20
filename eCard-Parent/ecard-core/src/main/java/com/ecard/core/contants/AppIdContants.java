c900cacb20de312ba5ab79d07a8dc08c8fce756a
HienTu <hientuminh@gmail.com>
2015-10-20 14:31:30 +0700

    [Push notification] change app id

--- eCard-Parent/ecard-core/src/main/java/com/ecard/core/contants/AppIdContants.java
+++ eCard-Parent/ecard-core/src/main/java/com/ecard/core/contants/AppIdContants.java
@@ -1,11 +1,11 @@
 package com.ecard.core.contants;
 
 public class AppIdContants {
-	
-	public static final String ANDROID_APP_ID = "ibs/ibs_android";
-//	public static final String ANDROID_APP_ID = "livepass4ads/12344321";  
-//	public static final String IOS_APP_ID = "livepass/ibs12345"; // test
-//	public static final String IOS_APP_ID = "livepass/ibs123456789"; // product
-	public static final String IOS_APP_ID = "ibs/ibs";
-//	public static final String IOS_APP_ID = "livepass/ibs";
+	// product ENV
+//	public static final String ANDROID_APP_ID = "ibs/ibs_android";
+//	public static final String IOS_APP_ID = "ibs/ibs";
+	// verify ENC
+	public static final String ANDROID_APP_ID = "ibs/ibs_androidtest";
+	public static final String IOS_APP_ID = "ibs/ibstest";
+
 }
