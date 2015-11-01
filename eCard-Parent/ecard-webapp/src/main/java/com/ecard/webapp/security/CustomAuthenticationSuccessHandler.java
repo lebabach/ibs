beb1b06b0b124554b6cf052c17f7857b28cac18f
Le Anh Vinh <vinhladn@gmail.com>
2015-11-01 11:12:06 +0700

    save history login

--- eCard-Parent/ecard-webapp/src/main/java/com/ecard/webapp/security/CustomAuthenticationSuccessHandler.java
+++ eCard-Parent/ecard-webapp/src/main/java/com/ecard/webapp/security/CustomAuthenticationSuccessHandler.java
@@ -5,21 +5,36 @@ package com.ecard.webapp.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import com.ecard.core.model.ActionLog;
+import com.ecard.core.model.ActionLogId;
+import com.ecard.core.model.enums.ActionLogType;
+import com.ecard.core.service.UserInfoService;
 
 /**
  * @author nhat.nguyen
  *
  */
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler  {
+	@Autowired
+    private UserInfoService userInfoService;
+	
+//	@Value("${msg.login.log}")
+//    private String msgLog;
 	
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -39,7 +54,19 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 		} else {
 			mainRoles.addAll(roles);
 		}
-		 
+
+		 //Save history
+		EcardUser ecardUser = (EcardUser) authentication.getPrincipal();
+        ActionLogId actionLogId = new ActionLogId();
+        actionLogId.setActionDate(new Date());
+        actionLogId.setActionMessage("ログイン");
+        actionLogId.setActionType(ActionLogType.LOGIN.getValue());
+        actionLogId.setUserId(ecardUser.getUserId());
+        
+        ActionLog actionLog = new ActionLog();
+        actionLog.setId(actionLogId);
+        userInfoService.saveActionLog(actionLog);
+        
 		String redirectUrl = RoleType.getRedirectUrlByRoleName(mainRoles.get(0));
 		String rootPath = request.getContextPath();
         if (redirectUrl == null){
